feca3cdca43dd810738eccbaaf368a9d3028df7f
양보윤 <boyunyang@naver.com>
2026-07-29 15:44:00 +0900

    feat : 회원가입 페이지 완성 (#16)
    * feat(Signup.vue) : 피그마 화면 1차 구현 완료
    
    * fix(Signup.vue) : 배치 간격 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
+// 회원가입 페이지
+import Signup from '@/pages/Signup.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage,
+    },
+    // 회원가입 페이지
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
     },
   ],
 });
