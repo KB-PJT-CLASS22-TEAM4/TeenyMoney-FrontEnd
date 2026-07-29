0b966b23f05a1ac3c789af6a8a6aa7735ef5c46a
Yang-Boyun <boyunyang@naver.com>
2026-07-29 15:37:55 +0900

    fix(Signup.vue) : 배치 간격 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
-import SignupPage from '@/pages/SignupPage.vue';
+// 회원가입 페이지
+import Signup from '@/pages/Signup.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,10 +11,11 @@ const router = createRouter({
       name: 'home',
       component: HomePage,
     },
+    // 회원가입 페이지
     {
       path: '/signup',
       name: 'signup',
-      component: SignupPage,
+      component: Signup,
     },
   ],
 });
