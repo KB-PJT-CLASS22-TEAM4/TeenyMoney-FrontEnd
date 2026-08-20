394927771f9c38d6e741b42d7cc9d6e052ed383c
we5046 <we5046@naver.com>
2026-08-20 10:36:15 +0900

    로컬 테스트 환경 -> 실제 배포 환경으로 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://www.teenymoney.kro.kr',
         changeOrigin: true,
         secure: true,
       },
