c084c1de8a2b2d2ddfd09c7b891249f673f39dbe
we5046 <we5046@naver.com>
2026-08-20 10:30:29 +0900

    feat(quest,SSE): 퀘스트 당일 생성 불가 문제 수정, 화면 SSE 반영 동기화 작업 실시
    해당 작업을 통해, 앞으로 사용자가 어플을 사용하고 있었다면 바로
    반영되어 새로고침을 하지 않아도 확인이 가능합니다.
    테스트한 영역: 송금, 오늘만 허용 요청, 퀘스트 생성

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-
   server: {
     proxy: {
       '/api': {
-        target: 'https://www.teenymoney.kro.kr',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: true,
-        timeout: 60000,
-        proxyTimeout: 60000,
       },
     },
   },
-})
+});
